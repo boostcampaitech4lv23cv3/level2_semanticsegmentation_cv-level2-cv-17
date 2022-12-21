7d29e5aeacc4b84c98646d699811a129cd000dd5
jkbin <wiken0072@naver.com>
2022-12-21 09:54:00 +0000

    :seedling: Add mmsegmentation files

---
+++ src/mmsegmentation/configs/_trash_/models/hrnet/fcn_hr18_512x1024_160k_cityscapes.py
@@ -0,0 +1,16 @@
+_base_ = [
+    #'../_base_/models/fcn_hr18.py', 
+    #'../_base_/datasets/cityscapes.py',
+    #'../_base_/default_runtime.py', 
+    #'../_base_/schedules/schedule_160k.py'
+    
+    '/opt/ml/input/mmsegmentation/configs/_trash_/models/hrnet/fcn_hr18.py',
+    '/opt/ml/input/mmsegmentation/configs/_trash_/default_runtime.py',
+    '/opt/ml/input/mmsegmentation/configs/_trash_/schedules/schedule_160k.py',
+    '/opt/ml/input/mmsegmentation/configs/_trash_/datasets/custom.py'
+    
+    
+    
+    
+    
+]

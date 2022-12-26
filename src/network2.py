6571e469abfe486d40269854feaf4d9c9137360b
jkbin <wiken0072@naver.com>
2022-12-26 02:47:01 +0000

    :wrench: Modifyloss,optim to network2.py

--- src/network2.py
+++ src/network2.py
@@ -6,19 +6,20 @@ import torch
 
     
 def smp_criterion():
-    return smp.losses.FocalLoss(mode='multiclass')
+    return nn.CrossEntropyLoss()
+
 
 def smp_model():
     model = smp.DeepLabV3Plus(
-        encoder_name='resnet18',
+        encoder_name='efficientnet-b0',
         encoder_weights='imagenet',
         in_channels=3,
         classes=11
     )
     return model
     
-def smp_optim(model,lr:float=1e-6):
-    return optim.AdamW(params=model.parameters(), lr=lr)
+def smp_optim(model,lr:float=0.0001):
+    return optim.Adam(params = model.parameters(), lr = lr, weight_decay=1e-6)
 
     
     
